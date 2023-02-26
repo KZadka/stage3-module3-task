d21e8d7a9377e1d2fc1f1f9eeb5fae2c8f82991e
KZadka <kzadka89@gmail.com>
2023-02-26 20:31:36 +0100

    Create TagRepo with CRUD operations

--- module-repository/src/main/java/com/mjc/school/repository/implementation/TagRepository.java
+++ module-repository/src/main/java/com/mjc/school/repository/implementation/TagRepository.java
@@ -1,34 +1,31 @@
 package com.mjc.school.repository.implementation;
 
 import com.mjc.school.repository.BaseRepository;
-import com.mjc.school.repository.model.implementation.NewsModel;
 import com.mjc.school.repository.model.implementation.TagModel;
-import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class NewsRepository implements BaseRepository<NewsModel, Long> {
+public class TagRepository implements BaseRepository<TagModel, Long> {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
-    public List<NewsModel> readAll() {
-        return entityManager.createQuery("SELECT n FROM NewsModel n", NewsModel.class)
+    public List<TagModel> readAll() {
+        return entityManager.createQuery("SELECT t FROM TagModel t", TagModel.class)
                 .getResultList();
     }
 
     @Override
-    public Optional<NewsModel> readById(Long id) {
-        return Optional.ofNullable(entityManager.find(NewsModel.class, id));
+    public Optional<TagModel> readById(Long id) {
+        return Optional.ofNullable(entityManager.find(TagModel.class, id));
     }
 
     @Override
-    public NewsModel create(NewsModel entity) {
+    public TagModel create(TagModel entity) {
         entityManager.getTransaction().begin();
         entityManager.persist(entity);
         entityManager.getTransaction().commit();
@@ -36,16 +33,10 @@ public class NewsRepository implements BaseRepository<NewsModel, Long> {
     }
 
     @Override
-    public NewsModel update(NewsModel entity) {
+    public TagModel update(TagModel entity) {
         entityManager.getTransaction().begin();
-        NewsModel updatedModel = entityManager.find(NewsModel.class, entity.getId());
-        updatedModel.setTitle(entity.getTitle());
-        updatedModel.setContent(entity.getContent());
-        updatedModel.setAuthorModel(entity.getAuthorModel());
-        List<TagModel> tagsToUpdate = entity.getTags();
-        if (!tagsToUpdate.isEmpty()) {
-            updatedModel.setTags(tagsToUpdate);
-        }
+        TagModel updatedModel = entityManager.find(TagModel.class, entity.getId());
+        updatedModel.setName(entity.getName());
         entityManager.getTransaction().commit();
         return updatedModel;
     }
@@ -54,7 +45,7 @@ public class NewsRepository implements BaseRepository<NewsModel, Long> {
     public boolean deleteById(Long id) {
         if (readById(id).isPresent()) {
             entityManager.getTransaction().begin();
-            entityManager.remove(entityManager.find(NewsModel.class, id));
+            entityManager.remove(entityManager.find(TagModel.class, id));
             entityManager.getTransaction().commit();
             return true;
         }
