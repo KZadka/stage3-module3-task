89b62335df29980854e56a7da7484d25b1f8de53
KZadka <kzadka89@gmail.com>
2023-03-04 20:28:18 +0100

    Create TagMapper

---
+++ module-service/src/main/java/com/mjc/school/service/mapper/TagMapper.java
@@ -0,0 +1,19 @@
+package com.mjc.school.service.mapper;
+
+import com.mjc.school.repository.model.implementation.TagModel;
+import com.mjc.school.service.dto.TagDtoRequest;
+import com.mjc.school.service.dto.TagDtoResponse;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+public interface TagMapper {
+
+    TagMapper INSTANCE = Mappers.getMapper(TagMapper.class);
+
+    TagDtoResponse tagModelToDto(TagModel tagModel);
+
+    @Mappings(value = {@Mapping(target = "createDate", ignore = true),
+            @Mapping(target = "lastUpdateDate", ignore = true)})
+    TagModel tagDtoToModel(TagDtoRequest tagDtoRequest);
+}
